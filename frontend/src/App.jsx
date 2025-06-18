4c7bd3f4397f23acb0cc455c565c87e725619c05
chetram <patelchetram49@gmail.com>
2025-06-18 08:12:16 +0530

    intial vite push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./components/Welcome"
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+
+        <Welcome />
+     
+    </div>
+  )
+}
+
+export default App
