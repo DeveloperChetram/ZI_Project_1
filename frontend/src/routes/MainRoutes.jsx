fb86e0203a2d579202b424947811f7173f72c811
chetram <patelchetram49@gmail.com>
2025-07-26 01:49:23 +0530

    working on profile section

--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import AdminDashboard from '../components/AdminDashboard';
 import Dashboard from '../pages/Dashboard';
+import ProfilePage from '../pages/ProfilePage'; // This path must match the file location
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -18,15 +19,20 @@ const MainRoutes = () => {
       <Route path='/home' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/sign-up' element={<Register />} />
-      <Route path='/admin-dashboard' element={<AdminDashboard />} />
-      {/* The /charts route has been removed */}
+
+      {/* --- Private Routes --- */}
+      {/* Wrap protected routes in the PrivateRoute component */}
       <Route
         path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
+        element={<PrivateRoute><Dashboard /></PrivateRoute>}
+      />
+      <Route
+        path="/admin-dashboard"
+        element={<PrivateRoute><AdminDashboard /></PrivateRoute>}
+      />
+      <Route
+        path="/profile"
+        element={<PrivateRoute><ProfilePage /></PrivateRoute>}
       />
     </Routes>
   );
